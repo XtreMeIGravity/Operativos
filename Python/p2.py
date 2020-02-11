0b73eb9a1039b9b6a7037fdd3d5130a2daf8362f
XtreMeiGravity <xtremeigraviti@hotmail.com>
2020-02-10 20:15:35 -0600

    PrimerCommit

---
+++ Python/p2.py
@@ -0,0 +1,9 @@
+# Import the module
+import subprocess
+# Ask the user for input
+host = rawnput("Enter a host to ping: ")	
+# Set up the echo command and direct the output to a pipe
+p1 = subprocess.Popen(['ping', '-c 2', host], stdout=subprocess.PIPE)cd
+# Run the command
+output = p1.communicate()[0]
+print (output)
