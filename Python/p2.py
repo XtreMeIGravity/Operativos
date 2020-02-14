c4bf019a9ead018c6b6d47b72e920c731429aa59
XtreMeIGravity <xtremeigraviti@hotmail.com>
2020-02-14 04:17:46 -0600

    cambios generales

--- Python/p2.py
+++ Python/p2.py
@@ -1,9 +1,20 @@
 import subprocess
-path="/home/xtreme/"
-s = subprocess.check_output(["find",path,"-type","f","-iname","*.pdf","-o","-name","*.mp4","-o","-name","*.jpg"]).decode("utf-8")
+path="$home"
+jpg=path+"jpg"
+pdf=path+"pdf"
+mp4=path+"mp4"
+s = subprocess.check_output(["find",path,"-type","f","-iname","*.pdf","-o","-name","*.mp4","-o","-name","*.jpg",
+"!","-path",jpg ,"!","-path", pdf,"!","-path", mp4]).decode("utf-8")
 rb=[s]
 rb=rb[0].split("\n")
 rb.pop(len(rb)-1)
+subprocess.run(["mkdir", "-p",jpg, pdf, mp4 ])
 for x in rb:
-    print(x)
-subprocess.run(["mkdir", "-p", path+"*.jpg", path+"*.pdf", path+"*.mp4" ])
+    if x.find(".jpg")>=0:
+        dest=jpg
+    elif x.find(".mp4")>=0:
+        dest=mp4
+    elif x.find(".pdf")>=0:
+        dest=pdf
+    subprocess.run(["cp",x,dest])
+subprocess.run(["tar", "-czvf", path+"TodoJunto.tar",jpg, pdf, mp4 ])
