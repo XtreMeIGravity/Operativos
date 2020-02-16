440f53c7c06188f7a0f32e6f0122684974bd61dd
XtreMeIGravity <xtremeigraviti@hotmail.com>
2020-02-15 19:15:36 -0600

    commit cambios generales

--- Python/p1.py
+++ Python/p1.py
@@ -1,8 +1,11 @@
 import subprocess
-path="/home/xtreme/"
-jpg=path+"jpg"
-pdf=path+"pdf"
-mp4=path+"mp4"
+from pathlib import Path
+
+path=str(Path.home())#Obtiene el path de home
+print(path)
+jpg=path+"/jpg"
+pdf=path+"/pdf"
+mp4=path+"/mp4"
 s = subprocess.check_output(["find",path,"-type","f","-iname","*.pdf","-o","-name","*.mp4","-o","-name","*.jpg"]).decode("utf-8")
 rb=[s]
 rb=rb[0].split("\n")
