d15b473d2d89587c6d463f3135c44a409c46de91
XtreMeIGravity <xtrmeigraviti@hotmail.com>
2020-06-08 01:44:05 -0500

    cambiosxD

--- PrimerDEP/Assambler/lesson15Carpeta/lesson15.asm
+++ PrimerDEP/Assambler/lesson15Carpeta/lesson15.asm
@@ -15,7 +15,7 @@ _start:
  
     mov     eax, 95     ; almacena un 90 en eax
     mov     ebx, 9      ; almacena un 9 en ebx
-    div     ebx         ; divide eax sobre ebx y lo almacena en eax
+    div     ebx         ; divide eax sobre ebx y lo almacena en eax y el residuo se queda en edx
     call    iprint      ; Imprime un entero 
     mov     eax, msg1   ; apunta el registro eax a msg ya que para sprint requiere que este en eax
     call    sprint      ; manda a llamar a la funcion sprint
